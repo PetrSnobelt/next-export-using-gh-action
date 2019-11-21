cb1d443911a92119fbaf601d5e21a211a8eaff3d
Petr Snobelt <petr.snobelt@kiwi.com>
2019-11-21 14:54:05 +0100

    try improve dynamic import route

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default () => {
         It Works!
       </Alert>
       <Illustration name="Success" />
-      <Link href={`${process.env.PREFIX}/dynamic-import`}>
+      <Link href="/dynamic-import" as={`${process.env.PREFIX}/dynamic-import`} >
         <a>Dynamic-import sample</a>
       </Link>
     </React.Fragment>
