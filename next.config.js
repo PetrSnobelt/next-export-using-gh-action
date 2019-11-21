fbc64b3bc53ace54823fafbc1bb1e05a63ce0478
Petr Snobelt <petr.snobelt@kiwi.com>
2019-11-21 12:34:14 +0100

    try remove  slash

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 console.log("process.env.GITHUB_ACTIONS", process.env.GITHUB_ACTIONS)
 
 module.exports = {
-  assetPrefix: process.env.GITHUB_ACTIONS ? "https://petrsnobelt.github.io/next-export-using-gh-action/" : "",
+  assetPrefix: process.env.GITHUB_ACTIONS ? "https://petrsnobelt.github.io/next-export-using-gh-action" : "",
 
   webpack(config, options) {
     config.resolve.alias['components'] = path.join(__dirname, 'components')
